08a4c6d4fe53af642198c06e6b699c0d4505d891
mitchellhoesing <hoesingmitch02@gmail.com>
2025-11-17 17:36:23 -0800

    Added unit test test_read_user_query()

--- tests/unit/test_BaseLLMClient.py
+++ tests/unit/test_BaseLLMClient.py
@@ -2,23 +2,20 @@ import unittest
 import sys
 import os
 
+from unittest.mock import patch
+
 # Add the src directory to the system path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
 
-from base.BaseLLMClient import BaseLLMClient
+from clients.OpenAIClient import OpenAIClient
 
 class TestBaseLLMClient(unittest.TestCase):
 
     def setUp(self):
-        self.llm_client = BaseLLMClient()
+        self.llm_client = OpenAIClient()
 
     def test_read_user_query(self):
-        # Since read_user_query uses input(), we will mock it for testing
-        input_values = ['What is the capital of France?']
-        def mock_input(s):
-            return input_values.pop(0)
-        
-        self.llm_client.input = mock_input
-        
-        user_query = self.llm_client.read_user_query()
-        self.assertEqual(user_query, 'What is the capital of France?')
+        # Mock the built-in input function
+        with patch('builtins.input', return_value='What is the capital of France?'):
+            user_query = self.llm_client.read_user_query()
+            self.assertEqual(user_query, 'What is the capital of France?')
